02ea85c69ee7b0f25345fe0429995a0c2f471e6f
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-06-05 18:02:29 +0530

    feat: sample modal

--- src/screens/home.component.js
+++ src/screens/home.component.js
@@ -1,19 +1,41 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
-import { Button, Layout } from "@ui-kitten/components";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { Button, Layout, Modal, Card, Text } from "@ui-kitten/components";
 
-export const HomeScreen = ({ navigation }) => {
+export const HomeScreen = () => {
   const navigateDetails = () => {
-    navigation.navigate("Details");
+    setVisible(true);
   };
+
+  const [visible, setVisible] = React.useState(false);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Layout
         style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
       >
-        <Button onPress={navigateDetails}>OPEN DETAILS</Button>
+        <Button onPress={navigateDetails}>OPEN MODAL</Button>
+
+        <Modal
+          visible={visible}
+          backdropStyle={styles.backdrop}
+          onBackdropPress={() => setVisible(false)}
+        >
+          <Card disabled={true}>
+            <Text>Welcome to DoppioMobile</Text>
+            <Button onPress={() => setVisible(false)}>DISMISS</Button>
+          </Card>
+        </Modal>
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 192,
+  },
+  backdrop: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+});
