53457a8a2787def33f11875a7c4a051eaae8a921
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-06-05 23:25:04 +0530

    chore: add note about redirect scheme

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,4 +1,7 @@
 export const BASE_URI = "https://apf-changemakers-staging.frappe.cloud";
-export const REDIRECT_URL_SCHEME = "io.frappe.changemakers";
+
+// must match the "scheme" property of the "app.json" file
+export const REDIRECT_URL_SCHEME = "io.frappe.changemakers"; 
+
 export const SECURE_AUTH_STATE_KEY = "AuthState";
 export const OAUTH_CLIENT_ID = "f592ecba60";
