c627983c0caad98584b3c99c09b32ab5076e3152
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-06-05 15:11:51 +0530

    feat: oauth works

--- App.js
+++ App.js
@@ -1,11 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, View, Text } from "react-native";
+import * as AuthSession from "expo-auth-session";
+
+const BASE_URI = "https://apf-changemakers-staging.frappe.cloud";
 
 export default function App() {
+  const redirectUri = AuthSession.makeRedirectUri({
+    scheme: "io.changemakers.app",
+    path: "auth",
+  });
+
+  console.log(redirectUri);
+
+  const [code, setCode] = useState(null);
+  const [token, setToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
+
+  const [request, response, promptAsync] = AuthSession.useAuthRequest(
+    {
+      clientId: "f592ecba60",
+      redirectUri,
+      responseType: "code",
+      scopes: ["all"],
+      usePKCE: false,
+    },
+    {
+      authorizationEndpoint: `${BASE_URI}/api/method/frappe.integrations.oauth2.authorize`,
+      tokenEndpoint: `${BASE_URI}/api/method/frappe.integrations.oauth2.get_token`,
+    }
+  );
+
+  useEffect(() => {
+    if (response?.type === "success" && !token) {
+      const { code } = response.params;
+
+      console.log("code: ");
+      console.log(code);
+
+      AuthSession.exchangeCodeAsync(
+        {
+          redirectUri,
+          code,
+          extraParams: {
+            grant_type: "authorization_code",
+            client_id: "f592ecba60",
+          },
+          clientId: "f592ecba60",
+        },
+        {
+          tokenEndpoint: `${BASE_URI}/api/method/frappe.integrations.oauth2.get_token`,
+        }
+      )
+        .then((res) => {
+          const { accessToken, refreshToken } = res;
+          setToken(accessToken);
+          setRefreshToken(refreshToken);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+
+      setCode(code);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        disabled={!request}
+        onPress={() => {
+          console.log("Triggering button click");
+          promptAsync();
+        }}
+        title="Login with Frappe"
+      />
+
+      <View>
+        <Text>Response: {JSON.stringify(token)}</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +85,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
