1c7d13c998378124614296cf1cdb5d2f394288b2
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-06-20 18:52:23 +0530

    feat: profile photo change and loading spinner

--- src/screens/details.screen.js
+++ src/screens/details.screen.js
@@ -1,11 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SafeAreaView } from "react-native";
 import styled from 'styled-components/native'
-import { Layout, Text, Button, Divider } from "@ui-kitten/components";
+import { Layout, Text, Button } from "@ui-kitten/components";
 import { AuthContext } from "../provider/auth";
 import * as ImagePicker from 'expo-image-picker';
 import { Image } from "expo-image"
 import uploadFile from "../utils/fileUploader";
+import { useFrappe } from "../provider/backend";
+import { CircularProgressBar } from '@ui-kitten/components';
 
 const LogoutButton = styled(Button)`
   border-radius: 6px;
@@ -19,19 +21,25 @@ const ProfileImage = styled(Image)`
 
 export const DetailsScreen = () => {
   const { isAuthenticated, logout, userInfo, accessToken, fetchUserInfo } = useContext(AuthContext);
+  const { db } = useFrappe();
+  const [loading, setLoading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Layout
         style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
       >
+        <Layout style={{ marginVertical: 20, position: "relative" }}>
+          <ProfileImage source={{
+            uri: userInfo.picture, headers: {
+              Authorization: `Bearer ${accessToken}` // for handling private images
+            }
+          }} contentFit="cover" />
 
+          {loading && <CircularProgressBar style={{ position: "absolute", top: 30, left: 30, backgroundColor: "white" }} progress={uploadProgress} />}
+        </Layout>
 
-        <ProfileImage source={{
-          uri: userInfo.picture, headers: {
-            Authorization: `Bearer ${accessToken}` // for handling private images
-          }
-        }} contentFit="cover" />
         <Button appearance="ghost" onPress={async () => {
           // implement change profile pic
           // let the user pick image from gallery
@@ -44,14 +52,20 @@ export const DetailsScreen = () => {
 
           if (!result.canceled) {
             try {
+              setLoading(true)
               await uploadFile(result.assets[0].uri, result.assets[0].uri.split('/').pop(), result.assets[0].type, {
                 accessToken: accessToken,
                 onUploadProgress: (progressEvent) => {
-                  console.log((progressEvent.loaded / progressEvent.total) * 100, '%');
+                  let progress = (progressEvent.loaded / progressEvent.total) * 100
+                  setUploadProgress(progress);
                 },
                 onUploadComplete: async (data) => {
-                  console.log("Upload Complete!")
+                  const fileUrl = data.message.file_url
+                  await db.updateDoc("User", userInfo.email, {
+                    user_image: fileUrl
+                  })
                   await fetchUserInfo()
+                  setLoading(false)
                 },
                 isPrivate: false,
                 doctype: "User",
@@ -60,6 +74,7 @@ export const DetailsScreen = () => {
               })
             } catch (error) {
               console.log(error)
+              setLoading(false)
             }
 
           }
